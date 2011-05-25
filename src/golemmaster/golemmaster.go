ad8df71af266767a59e51caccf4b5672672d8bd9
Ryan Bressler <ryanbressler@gmail.com>
2011-05-24 17:19:16 -0700

    basic server code

--- src/golemmaster/golemmaster.go
+++ src/golemmaster/golemmaster.go
@@ -17,3 +17,114 @@
    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
 
 */
+package main
+
+import (
+	"http"
+	"fmt"
+	"json"
+	"websocket"
+	//"io/ioutil"
+)
+
+var subidChan = make(chan int, 10)
+var jobChan = make(chan Job, 100)
+var nodeChan = make(chan Node, 100)
+
+type RequestedJob struct {
+	Count int
+	Args  []string
+}
+
+type Job struct {
+	SubId  int
+	LineId int
+	JobId  int
+	Args   []string
+}
+
+type Node struct {
+	Ws *websocket.Conn
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "This is a golem master node.")
+}
+
+func jobHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		fmt.Fprint(w, "Listing of jobs and statuses goes here")
+	case "POST":
+		go parseJobSub(r.FormValue("data"))
+		fmt.Fprint(w, "Job loaded.")
+	case "DEL":
+		fmt.Fprint(w, "Allow users to kill jobs here.")
+	}
+}
+
+func nodeHandler(ws *websocket.Conn) {
+	nodeChan <- Node{Ws: ws}
+}
+
+func parseJobSub(reqjson string) {
+	rJobs := make([]RequestedJob, 0, 100)
+	if err := json.Unmarshal([]byte(reqjson), &rJobs); err != nil {
+		fmt.Printf("%v", err)
+	}
+	subId := <-subidChan
+	subidChan <- subId + 1
+	jobId := 0
+	for lineId, vals := range rJobs {
+		for i := 0; i < vals.Count; i++ {
+			jobChan <- Job{SubId: subId, LineId: lineId, JobId: jobId, Args: vals.Args}
+		}
+	}
+
+}
+
+func monitorRemote(in chan string, idOnNode int) {
+
+}
+
+func runNode(node Node) {
+	/*//msg:=ioutil.ReadAll(node.ws)
+	//load ms from client
+	max:=3
+	inchans:=[max]chan string
+	for i:=0;i<max;i++{
+		go monitorRemote
+	}
+	running := false
+	var msg []byte
+	var job Job
+	for {
+		if running == false{
+			job=<-jobChan
+		}
+		msg=ioutil.ReadAll(node.ws)
+
+	}*/
+
+
+}
+
+func distribute() {
+	for {
+		node := <-nodeChan
+		go runNode(node)
+	}
+}
+
+func main() {
+	go distribute()
+	subidChan <- 0
+	fmt.Printf("Starting http Server ... ")
+	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/jobs/", jobHandler)
+	http.Handle("/distributor/", websocket.Handler(nodeHandler))
+	if err := http.ListenAndServe("0.0.0.0:8080", nil); err != nil {
+		fmt.Printf("ListenAndServe Error :" + err.String())
+	}
+
+}
