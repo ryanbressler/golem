47a08eb82bf9ec4055e716b1df639fb50698078b
Ryan Bressler <ryanbressler@gmail.com>
2011-05-31 16:27:20 -0700

    documentation for sample jobs

--- src/samplejobs/fileio.py
+++ src/samplejobs/fileio.py
@@ -16,13 +16,28 @@
 #    License along with this library; if not, write to the Free Software
 #    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
 # 
-# 
+#
+""" This is a sample golem job that generates random numbers and prints them to files.
+	
+	From the comand line it can be ran  the
+	sample, line and job ids:
+	fileio.py 0 0 0
+	
+	To run it six times using a golem master listening at
+	localhost:8083:
+	
+	golem.py localhost:8083 run 6 ~/Code/golem/src/samplejobs/fileio.py
+
+
+"""
 import random
 import sys
 
+#concactante the three args, the submission, line and job id's into one string for use 
+#constructing filenames
 id = ".".join(sys.argv[1:])
-print "script entered with id %s"%(id)
 
+# this could be the absolute path, otherwise it ends up where ever you started the nodes
 fo = open(id+".output.txt","w")
 for i in range(1000):
 	fo.write("%i\n"%(random.randint(0, 1000)))
--- src/samplejobs/stdio.py
+++ src/samplejobs/stdio.py
@@ -16,13 +16,34 @@
 #    License along with this library; if not, write to the Free Software
 #    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
 # 
-# 
+#############################################################################
+""" This is a sample golem job that generates random numbers and prints them to stdio.
+	
+	From the comand line it can be ran specifying the number of random numbers to simulate and the
+	sample, line and job ids:
+	stdio.py 3 0 0 0
+	
+	To run it six times generating three random numbers each using a golem master listening at
+	localhost:8083:
+	
+	golem.py localhost:8083 run 6 ~/Code/golem/src/samplejobs/stdio.py 3
+	
+	To run it using a list specifying provided list specifying diffrent paramaters:
+	golem.py localhost:8083 runlist ~/Code/golem/src/samplejobs/list.for.stdio.py.txt
+
+
+"""
 import random
 import sys
 
-id = ".".join(sys.argv[1:])
-#print "script entered with id %s"%(id)
+# the first arg is the number of times to simulate a random number
+n = int(sys.argv[1])
+
+#concactante the next three args, the submission, line and job id's into one string
+id = ".".join(sys.argv[2:])
 
 
-for i in range(3):
+
+for i in range(n):
+	#output the id string i and a random number. output looks like '4.0.4,2,611'
 	print "%s,%i,%i"%(id,i,random.randint(0, 1000))
