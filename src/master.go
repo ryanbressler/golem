2228c25a3c4f0a68702bff85157d9a8eb81deb30
Ryan Bressler <ryanbressler@gmail.com>
2011-06-09 17:55:22 -0700

    Password moved to header and brodcast support added.

--- src/master.go
+++ src/master.go
@@ -33,32 +33,31 @@ import (
 //master
 
 type Master struct {
-	subMap  map[int]*Submission //buffered channel for creating jobs
-	jobChan chan *Job //buffered channel for creating jobs
-	subidChan chan int//buffered channel for use as an incrementer to keep track of submissions
-	brodcastChans [] chan * clientMsg
-
+	subMap        map[int]*Submission //buffered channel for creating jobs
+	jobChan       chan *Job           //buffered channel for creating jobs
+	subidChan     chan int            //buffered channel for use as an incrementer to keep track of submissions
+	brodcastChans []chan *clientMsg
 }
 
-func NewMaster() * Master {
+func NewMaster() *Master {
 	m := Master{subMap: map[int]*Submission{},
-	jobChan : make(chan *Job, 0),
-	subidChan : make(chan int, 1),
-	brodcastChans : make( [] chan * clientMsg, 0, 0)}
+		jobChan:       make(chan *Job, 0),
+		subidChan:     make(chan int, 1),
+		brodcastChans: make([]chan *clientMsg, 0, 0)}
 	return &m
 
 }
 
 //web handlers
 //Handler for /. Nothing on root so say hello.
-func (m * Master) rootHandler(w http.ResponseWriter, r *http.Request) {
+func (m *Master) rootHandler(w http.ResponseWriter, r *http.Request) {
 	log("Root request.")
 	w.Header().Set("Content-Type", "text/plain")
 	fmt.Fprint(w, "Hello. This is a golem master node:\n http://code.google.com/p/golem/")
 }
 
 //restfull api for managing jobs handled on /jobs/
-func (m * Master)jobHandler(w http.ResponseWriter, r *http.Request) {
+func (m *Master) jobHandler(w http.ResponseWriter, r *http.Request) {
 	log("Jobs request.")
 
 	w.Header().Set("Content-Type", "text/plain")
@@ -68,7 +67,7 @@ func (m * Master)jobHandler(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Listing of Jobs Not Yet implemented.")
 	case "POST":
 		if usepw {
-			pw := hashPw(r.FormValue("password"))
+			pw := hashPw(r.Header.Get("Password"))
 			log("Verifying password.")
 			if hashedpw != pw {
 				fmt.Fprint(w, "Passwords do not match.")
@@ -88,11 +87,11 @@ func (m * Master)jobHandler(w http.ResponseWriter, r *http.Request) {
 
 
 //start routinges to manage nodes as they conect
-func (m * Master)nodeHandler(ws *websocket.Conn) {
-	log("Node connectiong from %v.",ws.LocalAddr().String())
-	bcChan := make( chan * clientMsg, 1)
-	m.monitorNode(NewConnection(ws),bcChan)
-	m.brodcastChans = append(brodcastChans,bcChan)
+func (m *Master) nodeHandler(ws *websocket.Conn) {
+	log("Node connectiong from %v.", ws.LocalAddr().String())
+	bcChan := make(chan *clientMsg, 1)
+	m.monitorNode(NewConnection(ws), bcChan)
+	m.brodcastChans = append(m.brodcastChans, bcChan)
 }
 
 
@@ -100,7 +99,7 @@ func (m * Master)nodeHandler(ws *websocket.Conn) {
 //wait for the Connection socket to not be in use then send it to 
 //the client. This may deadlock if the client is waiting for messages
 //so the client checks in. TODO: test if the InUse lock is needed.
-func (m * Master)sendJob(n *Connection, j *Job) {
+func (m *Master) sendJob(n *Connection, j *Job) {
 
 	con := *n
 	job := *j
@@ -117,7 +116,7 @@ func (m * Master)sendJob(n *Connection, j *Job) {
 
 //This waits for a handshake from a node then
 //monitors messages and starts jobs as needed
-func (m * Master)monitorNode(n *Connection,bcChan chan * clientMsg) {
+func (m *Master) monitorNode(n *Connection, bcChan chan *clientMsg) {
 	con := *n
 	nodename := con.Socket.LocalAddr().String()
 	//number to run at once
@@ -149,22 +148,22 @@ func (m * Master)monitorNode(n *Connection,bcChan chan * clientMsg) {
 			log("%v has %v running. Waiting for job or message.", nodename, running)
 			select {
 			case bcMsg := <-bcChan:
-				con.OutChan <- bcMsg
+				con.OutChan <- *bcMsg
 			case job := <-jobChan:
 				m.sendJob(&con, job)
 				running++
 			case msg = <-con.InChan:
 				//log("Got msg from %v", nodename)
-				running = m.clientMsgSwitch(nodename,&msg, running)
+				running = m.clientMsgSwitch(nodename, &msg, running)
 			}
 		default:
 			log("%v has %v running. Waiting for message.", nodename, running)
 			select {
 			case bcMsg := <-bcChan:
-				con.OutChan <- bcMsg
+				con.OutChan <- *bcMsg
 			case msg = <-con.InChan:
 				//log("Got msg from %v", nodename)
-				running = m.clientMsgSwitch(nodename,&msg, running)
+				running = m.clientMsgSwitch(nodename, &msg, running)
 			}
 		}
 
@@ -172,7 +171,7 @@ func (m * Master)monitorNode(n *Connection,bcChan chan * clientMsg) {
 
 }
 
-func (m * Master)clientMsgSwitch(nodename string, msg *clientMsg, running int) int {
+func (m *Master) clientMsgSwitch(nodename string, msg *clientMsg, running int) int {
 	switch msg.Type {
 	default:
 		//cout <- msg.Body
@@ -182,7 +181,7 @@ func (m * Master)clientMsgSwitch(nodename string, msg *clientMsg, running int) i
 	case CERROR:
 		m.subMap[msg.SubId].CerrFileChan <- msg.Body
 	case JOBFINISHED:
-		
+
 		log("%v says job finished: %v running: %v", nodename, msg.Body, running)
 		running--
 		m.subMap[msg.SubId].FinishedChan <- NewJob(msg.Body)
@@ -190,17 +189,16 @@ func (m * Master)clientMsgSwitch(nodename string, msg *clientMsg, running int) i
 
 	case JOBERROR:
 		log("%v says job error: %v running: %v", nodename, msg.Body, running)
-		running--		
+		running--
 		m.subMap[msg.SubId].ErrorChan <- NewJob(msg.Body)
 		log("%v finished sent to Sub: %v running: %v", nodename, msg.Body, running)
-
 
 	}
 	return running
 }
 
 
-func (m * Master)RunMaster(hostname string, password string) {
+func (m *Master) RunMaster(hostname string, password string) {
 	//start a server
 	subidChan <- 0
 	log("Running as master at %v", hostname)
@@ -214,9 +212,9 @@ func (m * Master)RunMaster(hostname string, password string) {
 		//tls.Listen("tcp", hostname, getTlsConfig()) 
 	}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){ m.rootHandler(w,r)})
-	http.HandleFunc("/jobs/", func(w http.ResponseWriter, r *http.Request){ m.jobHandler(w,r)})
-	http.Handle("/master/", websocket.Handler(func(ws *websocket.Conn){m.nodeHandler(ws)}))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { m.rootHandler(w, r) })
+	http.HandleFunc("/jobs/", func(w http.ResponseWriter, r *http.Request) { m.jobHandler(w, r) })
+	http.Handle("/master/", websocket.Handler(func(ws *websocket.Conn) { m.nodeHandler(ws) }))
 	if useTls {
 		cfg := getTlsConfig()
 		listener, err := tls.Listen("tcp", hostname, cfg)
