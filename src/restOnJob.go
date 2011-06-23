6b4ce3787a56897d91c95862a782124cf9763603
Hector Rovira <hrovira.isb@gmail.com>
2011-06-23 12:48:55 -0700

    cleanup up rest on job interface

--- src/restOnJob.go
+++ src/restOnJob.go
@@ -31,6 +31,8 @@ import (
 // REST interface for JobDispatchers
 type RestOnJob struct {
 	dispatcher JobDispatcher
+	hostname string
+	password string
 }
 
 type JobDispatcher interface {
@@ -42,32 +44,24 @@ type JobDispatcher interface {
 	Log(jobId string, w http.ResponseWriter)
 }
 
-// create a REST control node wrapping the given jobDispatcher
-func NewRestOnJob(jd JobDispatcher) *RestOnJob {
-	roj := RestOnJob{
-		dispatcher: jd,
-	}
-	return &roj
-}
-
 // initializes the REST control node
-func (j *RestOnJob) RunRestOnJob(hostname string, password string) {
-	log("running at %v", hostname)
+func (j *RestOnJob) MakeReady() {
+	log("running at %v", j.hostname)
 
-	if password != "" {
+	if j.password != "" {
 		usepw = true
-		hashedpw = hashPw(password)
+		hashedpw = hashPw(j.password)
+	}
+
+	//relys on global useTls being set
+	if err := ListenAndServeTLSorNot(j.hostname, nil); err != nil {
+		log("ListenAndServeTLSorNot Error : %v", err)
+		return
 	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { j.rootHandler(w, r) })
 	http.Handle("/html/", http.FileServer("html", "/html"))
 	http.HandleFunc("/jobs/", func(w http.ResponseWriter, r *http.Request) { j.jobHandler(w, r) })
-
-	//relys on global useTls being set
-	if err := ListenAndServeTLSorNot(hostname, nil); err != nil {
-		log("ListenAndServeTLSorNot Error : %v", err)
-		return
-	}
 }
 
 // web handlers
@@ -89,7 +83,7 @@ func (j *RestOnJob) jobHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
 		log("Method = GET.")
-		jobId, verb := parseJobRestUrl(r.URL.Path)
+		jobId, verb := parseJobUri(r.URL.Path)
 		switch {
 		case jobId != "":
 			fmt.Fprint(w, j.dispatcher.Retrieve(jobId))
@@ -111,7 +105,7 @@ func (j *RestOnJob) jobHandler(w http.ResponseWriter, r *http.Request) {
 			log("Password verified")
 		}
 
-		jobId, verb := parseJobRestUrl(r.URL.Path)
+		jobId, verb := parseJobUri(r.URL.Path)
 		switch {
 		case jobId != "" && verb == "stop":
 			fmt.Fprint(w, j.dispatcher.Stop(jobId))
@@ -123,53 +117,32 @@ func (j *RestOnJob) jobHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-//parser for rest jobs request
-func parseJobRestUrl(path string) (jobid string, verb string) {
-	pathParts := splitRestUrl(path)
-	nparts := len(pathParts)
-	jobid = ""
-	verb = ""
-	switch {
-	case nparts == 2:
-		jobid = pathParts[1]
-	case nparts == 3:
-		jobid = pathParts[1]
-		verb = pathParts[2]
-	}
-	vlog("Parsed job request id:\"%v\" verb:\"%v\"", jobid, verb)
-	return
+// Do Nothing JobDispatcher implementation
+type IKnowNothingJobDispatcher struct {
 }
-
-type SimpleJobDispatcher struct {
-
-}
-
-func (sjd SimpleJobDispatcher) RetrieveAll(params map[string]string) string {
+func (sjd IKnowNothingJobDispatcher) RetrieveAll(params map[string]string) string {
 	log("RetrieveAll")
 	return "{ items:[], numberOfItems: 0, uri:'/jobs' }"
 }
-func (sjd SimpleJobDispatcher) Retrieve(jobId string) string {
+func (sjd IKnowNothingJobDispatcher) Retrieve(jobId string) string {
 	log("Retrieve:%v", jobId)
 	return fmt.Sprintf("{ items:[], numberOfItems: 0, uri:'/jobs/%v' }", jobId)
 }
-func (sjd SimpleJobDispatcher) NewJob(params map[string]string) string {
+func (sjd IKnowNothingJobDispatcher) NewJob(params map[string]string) string {
 	log("NewJob")
 	jobId := UniqueId()
 	return sjd.Retrieve(jobId)
 }
-func (sjd SimpleJobDispatcher) Stop(jobId string) string {
+func (sjd IKnowNothingJobDispatcher) Stop(jobId string) string {
 	log("Stop:%v", jobId)
 	return sjd.Retrieve(jobId)
 }
-func (sjd SimpleJobDispatcher) Archive(jobId string) string {
+func (sjd IKnowNothingJobDispatcher) Archive(jobId string) string {
 	log("Archive:%v", jobId)
 	return sjd.Retrieve(jobId)
 }
-func (sjd SimpleJobDispatcher) Log(jobId string, w http.ResponseWriter) {
+func (sjd IKnowNothingJobDispatcher) Log(jobId string, w http.ResponseWriter) {
 	log("Log:%v", jobId)
 	w.Header().Set("Content-Type", "text/plain")
 	w.Write([]byte("Some output content\nYippe!!!"))
-}
-func NewSimpleJobDispatcher() JobDispatcher {
-	return SimpleJobDispatcher{}
 }
