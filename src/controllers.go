0b49e086715f63bba99f112cc4696034667bc644
Hector Rovira <hrovira.isb@gmail.com>
2011-07-14 14:19:52 -0700

    split controllers into two files

--- src/controllers.go
+++ src/controllers.go
@@ -21,7 +21,6 @@ package main
 
 import (
 	"http"
-	"fmt"
 	"strings"
 	"os"
 	"json"
@@ -179,77 +178,4 @@ func (c MasterNodeController) KillAll() os.Error {
 	go DieIn(3000000000)
 
 	return nil
-}
-
-type ScribeJobController struct {
-	scribe *Scribe
-}
-
-func (c ScribeJobController) RetrieveAll() (json string, numberOfItems int, err os.Error) {
-	log("RetrieveAll")
-	items, err := c.scribe.store.All()
-	if err != nil {
-		return
-	}
-
-	jsonArray := make([]string, 0)
-	for _, s := range items {
-		val, _ := s.MarshalJSON()
-		jsonArray = append(jsonArray, string(val))
-	}
-	numberOfItems = len(jsonArray)
-	json = strings.Join(jsonArray, ",")
-	err = nil
-	return
-}
-func (c ScribeJobController) Retrieve(jobId string) (json string, err os.Error) {
-	log("Retrieve:%v", jobId)
-	json = fmt.Sprintf("{ items:[], numberOfItems: 0, uri:'/jobs/%v' }", jobId)
-	err = nil
-	return
-}
-func (c ScribeJobController) NewJob(r *http.Request) (jobId string, err os.Error) {
-	reqjson := r.FormValue("data")
-	log("NewJob:%v", reqjson)
-	jobId = UniqueId()
-	err = nil
-	return
-}
-func (c ScribeJobController) Stop(jobId string) os.Error {
-	log("Stop:%v", jobId)
-	return os.NewError("unable to stop")
-}
-func (c ScribeJobController) Kill(jobId string) os.Error {
-	log("Kill:%v", jobId)
-	return os.NewError("unable to kill")
-}
-
-type ScribeNodeController struct {
-	scribe *Scribe
-}
-
-func (c ScribeNodeController) RetrieveAll() (json string, numberOfItems int, err os.Error) {
-	log("RetrieveAll")
-	json = "{ items:[], numberOfItems: 0, uri:'/nodes' }"
-	numberOfItems = 0
-	err = nil
-	return
-}
-func (c ScribeNodeController) Retrieve(nodeId string) (json string, err os.Error) {
-	log("Retrieve:%v", nodeId)
-	json = fmt.Sprintf("{ items:[], numberOfItems: 0, uri:'/nodes/%v' }", nodeId)
-	err = nil
-	return
-}
-func (c ScribeNodeController) RestartAll() os.Error {
-	log("Restart:")
-	return os.NewError("unable to restart")
-}
-func (c ScribeNodeController) Resize(nodeId string, numberOfThreads int) os.Error {
-	log("Resize:%v,%i", nodeId, numberOfThreads)
-	return os.NewError("unable to resize")
-}
-func (c ScribeNodeController) KillAll() os.Error {
-	log("Kill")
-	return os.NewError("unable to kill")
 }
