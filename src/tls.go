ac871983489e9719538830de727800a8deff6bc9
Hector Rovira <hrovira.isb@gmail.com>
2011-07-19 12:27:53 -0700

    removed isMaster dependency

--- src/tls.go
+++ src/tls.go
@@ -63,34 +63,28 @@ func getCertFilePaths() (string, string) {
 }
 
 //returns our custom tls configuration
-func getTlsConfig() (*tls.Config, os.Error) {
-	if certpath != "" {
-
-	}
+func getTlsConfig() (tlsConfig *tls.Config, err os.Error) {
 	certs := []tls.Certificate{}
-	if isMaster {
-		var cert tls.Certificate
-		var err os.Error
-		switch {
-		case certpath != "":
-			certf, keyf := getCertFilePaths()
-			cert, err = tls.LoadX509KeyPair(certf, keyf)
-			if err != nil {
-				vlog("Err loading tls keys from %v and %v: %v\n", certf, keyf, err)
-				return nil, err
-			}
-		default:
-			cert, err = GenerateTlsCert()
-			if err != nil {
-				log("Error generating tls cert: %v", err)
-			}
-		}
 
-		certs = append(certs, cert)
-	}
+    switch {
+    case certpath != "":
+        certf, keyf := getCertFilePaths()
+        cert, err := tls.LoadX509KeyPair(certf, keyf)
+        if err != nil {
+            vlog("Err loading tls keys from %v and %v: %v\n", certf, keyf, err)
+            return nil, err
+        }
+        certs = append(certs, cert)
+    default:
+        cert, err := GenerateTlsCert()
+        if err != nil {
+            log("Error generating tls cert: %v", err)
+        }
+        certs = append(certs, cert)
+    }
 
-	return &tls.Config{Certificates: certs, AuthenticateClient: true}, nil
-
+	tlsConfig = &tls.Config{Certificates: certs, AuthenticateClient: true}
+    return
 }
 
 //a replacment for ListenAndServeTLS that loads our custom confiuration usage is identical to http.ListenAndServe
